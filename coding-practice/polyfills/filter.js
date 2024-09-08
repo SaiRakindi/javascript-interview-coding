c1bc91a02ee0e11895f660aa841da9601f472402
SaiRakindi <sai.rakindi3@gmail.com>
2024-09-08 14:07:42 +0530

    feat: polyfill for filter

---
+++ coding-practice/polyfills/filter.js
@@ -0,0 +1,15 @@
+Array.prototype.myFilter = function (callbackFn) {
+  const filterResult = [];
+
+  for (let index = 0; index < this.length; index++) {
+    if (callbackFn(this[index], index)) filterResult.push(this[index]);
+  }
+
+  return filterResult;
+};
+
+const numbers = [1, 3, 4, 2, 56, 7, 8, 9];
+
+const customFilterResults = numbers.myFilter((num, index) => num % 2 === 0);
+
+console.log(customFilterResults);
