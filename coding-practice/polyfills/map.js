66a9f537abb2dcebe83f080d98a530d9c1e5125c
SaiRakindi <sai.rakindi3@gmail.com>
2024-09-08 13:58:36 +0530

    feat: polyfill for map

---
+++ coding-practice/polyfills/map.js
@@ -0,0 +1,17 @@
+const numsArray = [1, 2, 3, 4, 5];
+
+Array.prototype.myMap = function (callbackFn) {
+  let mapResult = [];
+
+  for (let index = 0; index < this.length; index++) {
+    mapResult.push(callbackFn(this[index], index));
+  }
+
+  return mapResult;
+};
+
+const numResult = numsArray.map((num, index) => num + index);
+const myMapResult = numsArray.myMap((num, index) => num ** index);
+
+console.log("myMapResult", myMapResult);
+console.log("numResult", numResult);
