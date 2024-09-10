d3558d546c44d1d62c4fe795b932e8fb9ed545d5
SaiRakindi <sai.rakindi3@gmail.com>
2024-09-10 23:47:39 +0530

    feat: another approach to check for palindrome

--- coding-practice/palindrome.js
+++ coding-practice/palindrome.js
@@ -2,15 +2,24 @@ function palindrome(inputString) {
   // solution using inbuilt functions
   //   return inputString.split("").reverse().join("") === inputString;
 
-  //brute force solution
-  let palindromeCheck = "";
-
-  for (let index = inputString.length - 1; index >= 0; index--) {
-    palindromeCheck += inputString[index];
+  //another appraoch
+  let stringLength = inputString.length;
+  for (let i = 0; i < stringLength / 2; i++) {
+    if (inputString[i] !== inputString[stringLength - i - 1]) {
+      return false;
+    }
   }
 
-  return palindromeCheck === inputString;
+  return true;
+
+  //brute force solution
+  //   let palindromeCheck = "";
+  //   for (let index = inputString.length - 1; index >= 0; index--) {
+  //     palindromeCheck += inputString[index];
+  //   }
+  //   return palindromeCheck === inputString;
 }
 
 console.log(palindrome("madam"));
+console.log(palindrome("madamimadam"));
 console.log(palindrome("sai"));
