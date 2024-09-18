ce9f6fb81416dbb80b3a8123c5dab74edf499da4
SaiRakindi <sai.rakindi3@gmail.com>
2024-09-18 11:08:40 +0530

    feat: polyfill for call method

---
+++ coding-practice/polyfills/call.js
@@ -0,0 +1,25 @@
+let car = {
+  color: "Red",
+  company: "Toyota",
+};
+
+function purchaseCar(price) {
+  console.log(
+    `I have purchased ${this.color} - ${this.company} car for ${price}`
+  );
+}
+
+//call usage
+purchaseCar.call(car, "500000");
+
+//polyfill
+Function.prototype.myCall = function (context = {}, ...rest) {
+  if (typeof this !== "function") {
+    throw new Error(this, ", is not callable");
+  }
+
+  context.func = this;
+  context.func(...rest);
+};
+
+purchaseCar.myCall(car, "5000");
